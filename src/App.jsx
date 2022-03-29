301c97e72ded978ec27fee8f265f5c764a6509da
venkata <sandeepsandy2530@gmail.com>
2022-03-29 22:44:41 +1100

    UI placed in form.

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,15 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br></br>
+        <form>
+          <input
+          type="text"
+          name="url"
+          placeholder="Github Repo link"
+          />
+          <button type="submit">Save URL</button>
+        </form>
       </div>
     );
   }
