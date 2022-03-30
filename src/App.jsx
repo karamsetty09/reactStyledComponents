45ed413cdf48ea4a5fcd650d6fd25c5f17be31ed
venkata <sandeepsandy2530@gmail.com>
2022-03-30 13:15:27 +1100

    axios, typescript and loading until url is fetched.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import react, { useState, Component } from 'react';
+import axios from 'axios';
+
+const config = {
+  method: 'get',
+  url: 'https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData',
+  headers: { 
+    'x-api-key': '7c3df78efe644c7ba9deb7238d6f0cdf'
+  }
+};
 
 class App extends Component{
+  state = {loading: true, url: null}
+  setUrl(goturl){
+    this.setState((url) => ({url: goturl}));
+  }
+  async componentDidMount() {
+    const res = await axios(config).then(function (response) {
+      console.log(JSON.stringify(response.data));
+      return(JSON.stringify(response.data.url));
+    })
+    console.log(res);
+    this.setState(Object.assign({loading: false, url: res}));
+  }
+  
   render(){
+    if(this.state.loading){
+      return <h2>Loading ...</h2>
+    }
+    const {url} = this.state;
     return (
       <div className="App">
         <br></br>
@@ -12,7 +37,7 @@ class App extends Component{
           type="text"
           name="url"
           placeholder="Github Repo link"
-          />
+          ></input>
           <button type="submit">Save URL</button>
         </form>
       </div>
