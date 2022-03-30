efd8ec24b00c591b9ce31af90b58fdefbc0c3544
venkata <sandeepsandy2530@gmail.com>
2022-03-30 18:19:37 +1100

    Handling POST Request.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import './App.css';
 import { Component } from 'react';
 import axios from 'axios';
 
-const config = {
+const getConfig = {
   method: 'get',
   url: 'https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData',
   headers: { 
     'x-api-key': '7c3df78efe644c7ba9deb7238d6f0cdf'
   }
+};
+
+let postConfig = {
+  method: 'post',
+  url: 'https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData',
+  headers: { 
+    'x-api-key': '7c3df78efe644c7ba9deb7238d6f0cdf', 
+    'Content-Type': 'application/json'
+  },
 };
 
 class App extends Component{
@@ -19,14 +28,33 @@ class App extends Component{
     this.onSubmitForm = this.onSubmitForm.bind(this);
   }
 
-  onSubmitForm(e) {
-    console.log(this._inputElemtent.value);
-    this._inputElemtent.value = "";
+  setPostConfig(gitHubLink){
+    var data = JSON.stringify({
+      "url": gitHubLink
+    });
+
+    const postConfigGit = { ...postConfig, data: data};
+
+    return postConfigGit;
+  }
+  async onSubmitForm(e) {
     e.preventDefault();
+    const postConfig = this.setPostConfig(this._inputElemtent.value);
+
+    // calling the axios post request 
+    const res = await axios(postConfig).then(function (response) {
+      return(JSON.stringify(response.data.url));
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+    console.log(res);
+    this._inputElemtent.value = "";
+    
   }
 
   async componentDidMount() {
-    const res = await axios(config).then(function (response) {
+    const res = await axios(getConfig).then(function (response) {
       return(JSON.stringify(response.data.url));
     })
     this.setState(Object.assign({loading: false, url: res}));
