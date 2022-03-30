63f0067cb64a751229315beb9dc94fcd96568b9e
venkata <sandeepsandy2530@gmail.com>
2022-03-31 01:21:14 +1100

    Refactored to functional Components, SASS and env files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,11 @@
-import './App.css';
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import './App.scss';
+import EditableInput from './components/EditableInput';
 
-const getConfig = {
-  method: 'get',
-  url: 'https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData',
-  headers: { 
-    'x-api-key': '7c3df78efe644c7ba9deb7238d6f0cdf'
-  }
-};
-
-let postConfig = {
-  method: 'post',
-  url: 'https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData',
-  headers: { 
-    'x-api-key': '7c3df78efe644c7ba9deb7238d6f0cdf', 
-    'Content-Type': 'application/json'
-  },
-};
-
-const App = () => {
-  const [url, setUrl] = useState("");
-
-  function setPostConfig(gitHubLink){
-    var data = JSON.stringify({
-      "url": gitHubLink
-    });
-
-    const postConfigGit = { ...postConfig, data: data};
-
-    return postConfigGit;
-  }
-
-  async function onSubmitForm(e) {
-    e.preventDefault();
-    const postConfig = setPostConfig(url);
-
-    // calling post to update endpoint. 
-    const res = await axios(postConfig).then(function (response) {
-      return response.data.url;
-    }).catch(function (error) {
-      console.log(error);
-    });
-    setUrl(res);
-  }
-
-  useEffect(() => {
-    getUrl();
-  }, []);
-
-  async function getUrl() {
-    // calling get before display of component
-    const res = await axios(getConfig).then(function (response) {
-      return response.data.url;
-    }).catch(function (error) {
-      console.log(error);
-    });
-    setUrl(res)    
-  }
+function App() {
     return (
       <div className="App">
         <br></br>
-        <form onSubmit={onSubmitForm}>
-          <input
-          type="text"
-          name="url"
-          placeholder="Github Repo link"
-          value={url}
-          onChange={myInput => setUrl(myInput.target.value)}
-          ></input>
-          <button type="submit" >Save URL</button>
-        </form>
+        <EditableInput/>
       </div>
     );
 };
---
+++ src/components/EditableInput.jsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useState, useEffect } from 'react'
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_BASE,
+    headers: {
+        'x-api-key': process.env.REACT_APP_BACKEND_API_KEY,
+        'content-type': 'application/json'
+    }
+})
+
+function EditableInput() {
+    const [url, seturl] = useState("")
+    const [urlError, seturlError] = useState(null)
+    const handleChange = (e) => {
+        seturl(e.target.value)
+    }
+    const saveUrl = async () => {
+        try {
+            let gitLink = new URL(url)
+            if (gitLink.href.length > 200) throw new Error('Url lentgh must be less than 200 characters.')
+            const savedUrl = await axiosInstance.post('', JSON.stringify({ url: gitLink.href }))
+            seturl(savedUrl.data.url)
+        } catch (err) {
+            console.warn(err)
+            seturlError(err.message)
+        }
+    }
+
+    const getUrl = useCallback(async () => {
+        const retrievedUrl = await (await axiosInstance.get()).data
+        seturl(retrievedUrl.url)
+    }, [seturl])
+
+    useEffect(() => {
+        getUrl()
+    }, [getUrl])
+
+    return (
+        <div className='myInputDiv'>
+            <input
+                placeholder='<your app code repo link>'
+                value={url}
+                onChange={handleChange}
+            />
+            {urlError && <span>{urlError}</span>}
+            <button onClick={saveUrl}>Save URL</button>
+        </div>
+    )
+}
+
+export default EditableInput
