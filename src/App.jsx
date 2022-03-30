67a4e5acd0ce2f880b2b89449a7caa50f9ad6fcc
venkata <sandeepsandy2530@gmail.com>
2022-03-30 19:11:20 +1100

    Work in progress.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Component } from 'react';
+import { useState, useEffect } from "react";
 import axios from 'axios';
 
 const getConfig = {
@@ -19,16 +19,10 @@ let postConfig = {
   },
 };
 
-class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      loading: true, url: null
-    };
-    this.onSubmitForm = this.onSubmitForm.bind(this);
-  }
+const App = () => {
+  const [url, setUrl] = useState("");
 
-  setPostConfig(gitHubLink){
+  function setPostConfig(gitHubLink){
     var data = JSON.stringify({
       "url": gitHubLink
     });
@@ -37,49 +31,54 @@ class App extends Component{
 
     return postConfigGit;
   }
-  async onSubmitForm(e) {
+
+  async function onSubmitForm(e) {
     e.preventDefault();
-    const postConfig = this.setPostConfig(this._inputElemtent.value);
+    console.log(e);
+    setUrl("");
+    // pass the correct vlaue to below line.
+    // const postConfig = setPostConfig();
 
     // calling the axios post request 
-    const res = await axios(postConfig).then(function (response) {
-      return(JSON.stringify(response.data.url));
-    }).catch(function (error) {
-      console.log(error);
-    });
+    // const res = await axios(postConfig).then(function (response) {
+    //   return(JSON.stringify(response.data.url));
+    // }).catch(function (error) {
+    //   console.log(error);
+    // });
 
-    console.log(res);
-    this._inputElemtent.value = "";
+    // console.log(res);
     
   }
 
-  async componentDidMount() {
+  useEffect(() => {
+    getUrl();
+  }, []);
+
+  async function getUrl() {
     const res = await axios(getConfig).then(function (response) {
       return(JSON.stringify(response.data.url));
     })
-    this.setState(Object.assign({loading: false, url: res}));
+    console.log(res);
+    setUrl(res)    
   }
+
   
-  render(){
-    if(this.state.loading){
-      return <h2>Loading ...</h2>
-    }
     return (
       <div className="App">
         <br></br>
-        <form onSubmit={this.onSubmitForm}>
+        <form onSubmit={onSubmitForm}>
           <input
           type="text"
           name="url"
           placeholder="Github Repo link"
-          ref={myInput => (this._inputElemtent = myInput)}
+          value={url}
+          onChange={myInput => setUrl(myInput.target.value)}
           ></input>
           <button type="submit" >Save URL</button>
         </form>
-        <h1>{this.state.value}</h1>
       </div>
     );
-  }
-}
+  // }
+};
 
 export default App;
