ab0150947e532798fc7edce0c3bee995814950a6
venkata <sandeepsandy2530@gmail.com>
2022-03-31 23:11:47 +1100

    Refactoring test cases like a documentation.

--- src/components/__test__/EditableInput.test.js
+++ src/components/__test__/EditableInput.test.js
@@ -2,40 +2,47 @@ import React from 'react';
 import {render, fireEvent, waitFor, wait, cleanup, screen} from '@testing-library/react'
 import EditableInput from '../EditableInput';
 
-it('check for textinput', () => {
-  render(<EditableInput />);
-  const textboxElement = screen.getByRole("textbox");
-  expect(textboxElement).toBeVisible();
+describe('Handling Input textbox.', ()=> {
+
+  it('check for textinput', () => {
+    render(<EditableInput />);
+    const textboxElement = screen.getByRole("textbox");
+    expect(textboxElement).toBeVisible();
+  });
+
+  it('gets data from endpoint', () => {
+    // Better approach would be by mocking the axios calls.
+    const axiosMock = {
+      get: jest.fn(),
+    }
+    axiosMock.get.mockReturnValueOnce({ data: { "url" :""} })
+    render(<EditableInput/>);
+    const inputElement = screen.getByPlaceholderText(/<your app code repo link>/i)
+    expect(inputElement.value).toBe("");
+  });
+  
+  it('should be able to edit the url before posting', () => {
+    render(<EditableInput/>);
+    const inputElement = screen.getByPlaceholderText(/<your app code repo link>/i)
+    fireEvent.change(inputElement, { target: {value: "https://github.com/karamsetty09/vdaq/changed"} });
+    expect(inputElement.value).toBe("https://github.com/karamsetty09/vdaq/changed");
+  });
 });
 
-it('Checks for button', () => {
+describe('Submit button to Handle post.', ()=> {
+
+  it('Checks for button', () => {
     render(<EditableInput />);
     const ButtonElement = screen.getByText(/Save URL/);
     expect(ButtonElement).toBeVisible();
   });
 
-it('gets data from endpoint', async () => {
-  // const axiosMock = {
-  //   get: jest.fn(),
-  // }
-  // axiosMock.get.mockReturnValueOnce({ data: { "url" :""} })
-  // const inputElement = await screen.findByTestId("submit-url-link")
-  // console.log(inputElement.value);
-  // render(<EditableInput/>);
-});
-
-it('should be able to edit the url before posting', () => {
-  render(<EditableInput/>);
-  const inputElement = screen.getByPlaceholderText(/<your app code repo link>/i)
-  fireEvent.change(inputElement, { target: {value: "https://github.com/karamsetty09/vdaq/changed"} });
-  expect(inputElement.value).toBe("https://github.com/karamsetty09/vdaq/changed");
-});
-
-it('posts data to endpoint', () => {
-  render(<EditableInput/>);
-  const inputElement = screen.getByPlaceholderText(/<your app code repo link>/i)
-  const buttonElement = screen.getByRole("button")
-  fireEvent.change(inputElement, { target: {value: "https://github.com/karamsetty09/vdaq/changed"} });
-  fireEvent.click(buttonElement);
-  expect(inputElement.value).not.toBe("https://github.com/karamsetty09/vdaq");
+  it('posts data to endpoint', () => {
+    render(<EditableInput/>);
+    const inputElement = screen.getByPlaceholderText(/<your app code repo link>/i)
+    const buttonElement = screen.getByRole("button")
+    fireEvent.change(inputElement, { target: {value: "https://github.com/karamsetty09/vdaq/changed"} });
+    fireEvent.click(buttonElement);
+    expect(inputElement.value).not.toBe("https://github.com/karamsetty09/vdaq");
+  });
 });
